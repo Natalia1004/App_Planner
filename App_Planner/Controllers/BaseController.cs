bc654e18b0e6f888f97bee5daa2855ad76988f5c
Natalia1004 <57062299+Natalia1004@users.noreply.github.com>
2021-03-15 10:13:22 +0100

    First commit

---
+++ App_Planner/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using App_Planner.Models;
+
+
+namespace App_Planner.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected void Flash(string message, string debug = null)
+        {
+            var alerts = TempData.ContainsKey(Alert.AlertKey) ?
+                (List<Alert>)TempData[Alert.AlertKey] :
+                new List<Alert>();
+
+            alerts.Add(new Alert
+            {
+                Message = message,
+                Debug = debug
+            });
+
+            TempData[Alert.AlertKey] = alerts;
+        }
+    }
+}
---
+++ App_Planner/Models/Alert.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace App_Planner.Models
+{
+    public class Alert
+    {
+        public const string AlertKey = "TempDataAlerts";
+        public string Message { get; set; }
+        public string Debug { get; set; }
+    }
+}
